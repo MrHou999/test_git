1a88844822ed082e76a4b626b73094eb01b04fa8
MrHou999 <mrhoudm@163.com>
2020-03-15 16:22:43 +0800

    My first commit git_test

---
+++ src/main/java/com/yfas/git/test_git/App.java
@@ -0,0 +1,13 @@
+package com.yfas.git.test_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/yfas/git/test_git/gitest.java
@@ -0,0 +1,10 @@
+package com.yfas.git.test_git;
+
+public class gitest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
